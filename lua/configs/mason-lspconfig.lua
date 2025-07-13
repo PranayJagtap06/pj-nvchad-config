0dfea53c9e464642450a4598e19a67f4c25e3bc3
PranayJagtap06 <pranays.jagtap@gmail.com>
2025-07-13 19:26:58 +0530

    4th commit in main-updated mason-lspconfig.lua.

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,27 +1,23 @@
-local lspconfig = package.loaded["lspconfig"]
+-- import mason-lspconfig plugin safely
+local mason_lspconfig = require("mason-lspconfig")
 
--- List of servers to ignore during install
-local ignore_install = {}
+-- list of servers for mason to install
+local servers = {
+	"lua_ls",
+	"pyright",
+	"ruff_lsp",
+	"black",
+	"isort",
+	"bashls",
+	"dockerls",
+	"jsonls",
+	"marksman",
+	"prettier",
+	"tailwindcss",
+	"yamlls",
+}
 
--- Helper function to find if value is in table.
-local function table_contains(table, value)
-    for _, v in ipairs(table) do
-        if v == value then
-            return true
-        end
-    end
-    return false
-end
-
--- Build a list of lsp servers to install minus the ignored list.
-local all_servers = {}
-for _, s in ipairs(lspconfig.servers) do
-    if not table_contains(ignore_install, s) then
-        table.insert(all_servers, s)
-    end
-end
-
-require("mason-lspconfig").setup({
-    ensure_installed = all_servers,
-    automatic_installation = false,
+-- setup mason-lspconfig
+mason_lspconfig.setup({
+	ensure_installed = servers,
 })
